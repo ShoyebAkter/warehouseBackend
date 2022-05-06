d485d80420a951dbfe8a51cc7016a381287670f3
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-05-06 12:11:40 +0600

    heroku and api created

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run(){
 
         app.get('car/:id',async(req,res)=>{
             const id=req.params.id;
+            console.log(id)
             const query={_id:ObjectId(id)};
             const car=await carCollection.findOne(query);
             res.send(car);
